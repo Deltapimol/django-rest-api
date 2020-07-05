3630ade061defb50ba23820aef82c43ac5f4bfeb
Deltapimol <43440596+Deltapimol@users.noreply.github.com>
2020-07-05 15:18:19 +0530

    Created class based views for article list and article detail urls using APIView.

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -1,23 +1,23 @@
 from rest_framework import serializers
 from .models import Article
 
+
 class ArticleSerializer(serializers.Serializer):
-    
-     title = serializers.CharField(max_length=100)
-     author = serializers.CharField(max_length=100)
-     email = serializers.EmailField(max_length=100)
-     date = serializers.DateTimeField()
-    
-     def create(self, validated_data):
-         return Article.objects.create(**validated_data)
-    
-     def update(self, instance, validated_data):
-         instance.title = validated_data.get('title', instance.title)
-         instance.author = validated_data.get('author', instance.author)
-         instance.email = validated_data.get('email', instance.email)
-         instance.date = validated_data.get('date', instance.date) 
-         instance.save()
-         return instance
+    title = serializers.CharField(max_length=100)
+    author = serializers.CharField(max_length=100)
+    email = serializers.EmailField(max_length=100)
+    date = serializers.DateTimeField()
+
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.author = validated_data.get('author', instance.author)
+        instance.email = validated_data.get('email', instance.email)
+        instance.date = validated_data.get('date', instance.date)
+        instance.save()
+        return instance
 
 # Can use ModelSerializer as an alternative. Compact version of Serializer. Gives the same result.
 
@@ -25,5 +25,3 @@ class ArticleSerializer(serializers.Serializer):
 #     class Meta:
 #         model = Article
 #         fields = ('id','title','author','email')
-        
-        
--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetailAPIView
 
-urlpatterns= [
-    path('article/', article_list),
-    path('article/<int:pk>/', article_detail),
+urlpatterns = [
+    # Function based view path for article list
+    # path('article/', article_list),
+
+    # Class based view path for article list
+    path('article/', ArticleAPIView.as_view()),
+
+    # Function based view path for article detail
+    # path('article/<int:pk>/', article_detail),
+
+    # Class based view path for article detail
+    path('article/<int:pk>/', ArticleDetailAPIView.as_view())
 ]
